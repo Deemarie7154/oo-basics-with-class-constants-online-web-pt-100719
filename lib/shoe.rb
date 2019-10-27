0ce6510990157e2cac3bf5f6b69b7c434e0f20f6
Dwight Marie <dwightmarie@gmail.com>
2019-10-27 20:22:55 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,7 @@ BRANDS = []
 
   def initialize(brand)
     @brand = brand
+     BRANDS << brand unless BRANDS.any? { |b| b == brand }
   end
  
  
@@ -15,9 +16,5 @@ BRANDS = []
     puts "Your shoe is as good as new!"
   end
 
-def brand=(brand) 
-  @brand = brand
-  BRANDS << brand
-  end
 end
 
